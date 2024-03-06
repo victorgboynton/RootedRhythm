6225c6a76d91f86adcf4f8f83c27446d4595552c
victorgboynton <victorgboynton@gmail.com>
2024-03-06 14:08:35 -0700

    Started to populate the about section with content, and laid out general structure

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export default function About() {
-	return <div className="pt-20">This is the About Page</div>;
+	return (
+		<div className="pt-20 text-white font-Anta">
+			<div className="text-center text-[60px] leading-[97px]">
+				<h1>We Are</h1>
+				<div className="relative w-[300px] h-[400px] sm:w-[400px] sm:h-[500px] mx-auto animate-[fadeIn_2s_ease]">
+					<Image
+						src="/rootedRhythmLogoInverted.webp"
+						alt="Rooted Rhythm Logo"
+						fill
+					></Image>{" "}
+				</div>
+			</div>
+			<div></div>
+		</div>
+	);
 }
