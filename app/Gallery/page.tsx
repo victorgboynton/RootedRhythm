9856269c9d7a229d615eeeb9fbf9407ccc29b902
victorgboynton <victorgboynton@gmail.com>
2024-03-10 14:22:57 -0600

    Added component for the gallery images, along with a type

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Gallery() {
-  return (
-    <div>
-      <h1>This is the gallery</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>This is the gallery</h1>
+		</div>
+	);
+}
+type GalleryComponentProps = {
+	size: string;
+	picture: string;
+	alt: string;
+};
+function GalleryComponent({ size, picture, alt }: GalleryComponentProps) {
+	return (
+		<div className={`relative ${size}`}>
+			<Image src={picture} alt={alt} fill></Image>
+		</div>
+	);
 }
