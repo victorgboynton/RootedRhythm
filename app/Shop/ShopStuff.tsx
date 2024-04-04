e5eb0e6d8daf5172c07e624e2a24ebbc2fe58a51
victorgboynton <victorgboynton@gmail.com>
2024-04-03 19:36:42 -0600

    Added styling options to the storefront

--- app/Shop/ShopStuff.tsx
+++ app/Shop/ShopStuff.tsx
@@ -38,13 +38,84 @@ export default function ShopStuff() {
 				storefrontAccessToken: "your-storefront-access-token",
 			});
 
-			ShopifyBuy.UI.onReady(client).then((ui) => {
+			ShopifyBuy.UI.onReady(client).then((ui: any) => {
 				ui.createComponent("collection", {
 					id: "your-collection-id",
 					node: document.getElementById("collection-component-1711452585861"),
 					moneyFormat: "%24%7B%7Bamount%7D%7D",
 					options: {
-						// Customize these options as needed
+						product: {
+							iframe: false, // Set to false to embed the product directly into your page instead of using an iframe
+							buttonDestination: "modal", // Specifies where to direct the user after clicking the button: "cart", "checkout", or "modal"
+							variantId: "all",
+							width: "240px",
+							contents: {
+								img: false,
+								imgWithCarousel: true,
+								title: false,
+								variantTitle: false,
+								price: true,
+								description: true,
+								buttonWithQuantity: false,
+								quantity: false,
+							},
+							styles: {
+								product: {
+									"@media (min-width: 601px)": {
+										"max-width": "calc(25% - 20px)",
+										"margin-left": "20px",
+										"margin-bottom": "50px",
+										width: "calc(25% - 20px)",
+									},
+								},
+							},
+						},
+						cart: {
+							contents: {
+								button: true,
+							},
+							styles: {
+								button: {
+									"background-color": "#ff6700",
+									":hover": {
+										"background-color": "#e65c00",
+									},
+									"border-radius": "2px",
+									":focus": {
+										"background-color": "#e65c00",
+									},
+								},
+								footer: {
+									"background-color": "#ffffff",
+								},
+							},
+						},
+						modalProduct: {
+							contents: {
+								img: false,
+								imgWithCarousel: true,
+								button: false,
+								buttonWithQuantity: true,
+							},
+							styles: {
+								product: {
+									"@media (min-width: 601px)": {
+										"max-width": "100%",
+										"margin-left": "0px",
+										"margin-bottom": "0px",
+									},
+								},
+							},
+						},
+						productSet: {
+							styles: {
+								products: {
+									"@media (min-width: 601px)": {
+										"margin-left": "-20px",
+									},
+								},
+							},
+						},
 					},
 				});
 			});
